fc998d5e72f9d7516add26e0a7e2e6aab492523a
Trestan Simon <trestansimon@gmail.com>
2023-02-18 17:58:14 -0500

    Improve camera handling for plane

--- Assets/Scripts/Surfaces/Plane.cs
+++ Assets/Scripts/Surfaces/Plane.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 using static Unity.Mathematics.math;
 
 public class Plane : Complex {
+    public Vector3 center;
+
     public override void Awake() {
         sideCount = 2;
+        UpdateCamera(true);
     }
     
     public override void GenerateVertices() {
@@ -15,9 +18,35 @@ public class Plane : Complex {
         for (int u = 0; u <= ResU; u++) {
             for (int v = 0; v <= ResV; v++) {
                 vertices[u,v] = new Vector3(
-                    u, v, 0
+                    u, 0, v
                 );
             }
+        }
+    }
+
+    public override void UpdateCamera(bool force = false) {
+        cam = Camera.main;
+        center = new Vector3(ResU/2f, 0f, ResV/2f);
+
+        if (force) {
+            cam.transform.position = center + 30f*Vector3.up;
+            cam.transform.rotation = Quaternion.Euler(90f, 0f, 270f);
+        }
+
+        scroll = Input.mouseScrollDelta.y * sensitivity * -1f;
+
+        if (Input.GetMouseButtonDown(2)) {
+            mousePos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2) && mousePos != null) {
+            dmousePos = Input.mousePosition - mousePos;
+            cam.transform.RotateAround(center, Vector3.up, dmousePos.x/2f*Time.deltaTime);
+            cam.transform.RotateAround(center, Camera.main.transform.right, -dmousePos.y/2f*Time.deltaTime);
+        }
+        if (scroll != 0f) {
+            FOV += scroll;
+            FOV = Mathf.Clamp(FOV, minFOV, maxFOV);
+            cam.fieldOfView = FOV;
         }
     }
 }
